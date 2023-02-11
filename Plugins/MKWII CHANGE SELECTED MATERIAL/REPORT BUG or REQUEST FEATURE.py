3df154cf4b88a5983b7de6d34f9332ab7ee17a23
ABCCBAYXZ <113473867+ABCCBAYXZ@users.noreply.github.com>
2023-02-11 17:05:48 +0100

    Add files via upload

---
+++ Plugins/MKWII CHANGE SELECTED MATERIAL/REPORT BUG or REQUEST FEATURE.py
@@ -0,0 +1,13 @@
+__author__ = "Okin"
+__version__ = "1.0"
+
+from BrawlCrate.API import *
+from BrawlLib.SSBB.ResourceNodes import *
+from System.Windows.Forms import *
+
+
+
+if BrawlAPI.ShowOKCancelPrompt("visit https://github.com/ABCCBAYXZ/brawlcrate-change-selected-materials-plugin/issues\n and create a new issue to report an bug or request a new feature you want to see.\nyou need a github account for this. \n\nPress OK to copy the report link to your clipboard", "copy report link to clipboard?"):
+    Clipboard.SetText("https://github.com/ABCCBAYXZ/brawlcrate-change-selected-materials-plugin/issues")
+
+
